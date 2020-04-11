2cc27b9a27b41d3c0377e690ee18d5452b00d812
stevehind <steve.hind@gmail.com>
2020-04-11 12:57:12 -0700

    Fixed issue with H2H Leaderboard loading.

--- src/H2HPage.js
+++ src/H2HPage.js
@@ -91,7 +91,6 @@ class H2HPage extends Component<Props, State> {
     componentDidMount() {
         api.getPlayers(
             (result: AxiosResponse<PlayerObjectList>) => {
-                console.log("Raw result was: %o", result);
                 this.setState({players: result.data});
             }
         );
@@ -113,6 +112,9 @@ class H2HPage extends Component<Props, State> {
     }
 
     computeWinChance(names: PairOfNames, data: PairOfRatings): PairOfWinPctTDs{
+        console.log("Data are: %o", data);
+        console.log("names are: %o", names);
+
         // $FlowFixMe
         const list_of_r: PairOfR = data.map((rating: number) => Math.pow(10, (rating / 400)))
 
@@ -129,13 +131,22 @@ class H2HPage extends Component<Props, State> {
 
         const dummy_list: Array<number> = [0,1];
 
-        // $FlowFixMe
+        // $FlowFixMe   
         const e_and_names: PairOfNameAndWinPct = dummy_list.map((i: number) => {
             return names[i].concat(": ").concat(formatted_e[i]);
         });
 
         // $FlowFixMe
         return e_and_names.map(item => <td className="text-center">{item}</td>);
+    }
+
+    notReadyToLoadResult (elos: ?Elos, table_data: ?TableObjectList, player_1: ?string, player_2: ?string): boolean {
+        // $FlowFixMe
+        if (elos === undefined || table_data === undefined || player_1 === undefined || player_2 === undefined || elos.length === 0 ) {
+            return true
+        } else {
+            return false
+        }
     }
 
     async submit(ev: SyntheticEvent<>) {
@@ -192,48 +203,48 @@ class H2HPage extends Component<Props, State> {
             </form>
             {
                 ((this.state.player_1 === undefined) || (this.state.player_2 === undefined)) ?
-                <button
-                    className="muted-button"
-                    onClick={this.submit}
-                    disabled={((this.state.player_1 === undefined) || (this.state.player_2 === undefined))}
-                >Choose two players</button>
+                    <button
+                        className="muted-button"
+                        onClick={this.submit}
+                        disabled={((this.state.player_1 === undefined) || (this.state.player_2 === undefined))}
+                    >Choose two players</button>
                 :
-                <button
-                    className="button"
-                    onClick={this.submit}
-                    disabled={((this.state.player_1 === undefined) || (this.state.player_2 === undefined))}
-                >Calculate!</button>
+                    <button
+                        className="button"
+                        onClick={this.submit}
+                        disabled={((this.state.player_1 === undefined) || (this.state.player_2 === undefined))}
+                    >Calculate!</button>
             }
             {
                 this.state.submitted === true ?
-                <div>
-                <div>
-                    <h4>Elo-Implied Win Probabilities in Next Game</h4>
-                    {
-                        this.state.table_data === undefined || this.state.player_1 === undefined || this.state.player_2 === undefined || this.state.elos === undefined ?
-                            <p><em>Loading win probabilities...</em></p>
-                            :
-                            <table>
-                            <tr>
-                                {this.computeWinChance(
-                                    // $FlowFixMe
-                                    Object.values(this.state.elos).map((value: Elo) => value.name),
-                                    // $FlowFixMe
-                                    Object.values(this.state.elos).map((value: Elo) => value.elo)
-                                )}
-                            </tr>
-                            </table>
-                    } 
-                </div>
-                    <h4>Head to Head Leaderboard</h4>
-                    {
-                        this.state.table_data === undefined || this.state.player_1 === undefined || this.state.player_2 === undefined || this.state.elos === undefined ? 
-                            <p><em>Loading leaderboard...</em></p>
-                            :
-                            <H2HTable tableData={this.state.table_data} className="alternate-background"/>    
-                    }
-                <p>Players are ranked by their <a href='https://en.wikipedia.org/wiki/Elo_rating_system'>Elo rating</a>.</p>
-            </div>
+                    <div>
+                        <div>
+                            <h4>Elo-Implied Win Probabilities in Next Game</h4>
+                            {
+                                this.notReadyToLoadResult(this.state.elos, this.state.table_data, this.state.player_1, this.state.player_2) ?
+                                    <p><em>Loading win probabilities...</em></p>
+                                :
+                                    <table>
+                                        <tr>
+                                            {this.computeWinChance(
+                                                // $FlowFixMe
+                                                Object.values(this.state.elos).map((value: Elo) => value.name),
+                                                // $FlowFixMe
+                                                Object.values(this.state.elos).map((value: Elo) => value.elo)
+                                            )}
+                                        </tr>
+                                    </table>
+                            } 
+                            <h4>Head to Head Leaderboard</h4>
+                            {
+                                this.notReadyToLoadResult(this.state.elos, this.state.table_data, this.state.player_1, this.state.player_2) ? 
+                                    <p><em>Loading leaderboard...</em></p>
+                                :
+                                    <H2HTable tableData={this.state.table_data} className="alternate-background"/>    
+                            }
+                            <p>Players are ranked by their <a href='https://en.wikipedia.org/wiki/Elo_rating_system'>Elo rating</a>.</p>
+                        </div>
+                    </div>
                 :
                 <p>Choose two players and then click "Calculate!" to see stats.</p>
             }
